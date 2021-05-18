b4b5cbdee9a0d25e4a90a9a3e5a5626f8368d588
LachlanMatt <46397335+LachlanMatt@users.noreply.github.com>
2021-05-18 12:19:34 +1000

    0.5 Run file
    Version 0.5
    Added a dedicated file to run the program

---
+++ RunDetector.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 12:00:07 2021
+
+@author: Lachlan
+"""
+
+###################################
+
+import cv2
+import numpy as np
+import pytesseract
+import os
+import time
+import re
+import ProcessingElements as PE
+
+###################################
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+timestr = time.strftime("%Y-%m-%d_%H-%M-%S")
+inputPath = "./input/"
+inputType = ["png", "jpg", "jpeg"]
+testPath='./testcases/'
+outputPath='./output/'
+outputType='.csv'           # .txt or .csv supported
+debuggingPath=outputPath+'Result_'+timestr+"/"
+imageWidthLim = [400, 800]  # max should be 2xmin+
+debugging = False           #enable view and save of intermediary images and text
+
+###################################
+
+#run the main program
+PE.runProgram()
